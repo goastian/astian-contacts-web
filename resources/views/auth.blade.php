5ab9784f4fa7d482039873e07ebbe77ab1ae087e
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2024-02-16 13:14:05 -0500

    Updated auth view

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -20,11 +20,11 @@
     <div class="container">
         <div class="card mt-5">
             <div class="card-head mb-5 text-center fw-bold h1 text-secondary">
-                Iniciar Session Con
+                {{__('Login')}}
             </div>
             <div class="card-body text-center text-capitalize mt-5">
                 <form action="/redirect" method="GET">
-                    <button type="submit" class="btn btn-primary">Login With Server</button>
+                    <button type="submit" class="btn btn-primary">{{__('Login With Server')}}</button>
                 </form>
             </div>
         </div>
